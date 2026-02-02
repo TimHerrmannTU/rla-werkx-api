ed56eb9a8646482dec4e82203797462b657eea41
TimHerrmannTU <platinum2dollar@gmail.com>
2026-02-02 11:22:36 +0100

    Added teams table

---
+++ models/team.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Team(Base):
+    __tablename__ = "teams"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20))
+    lead_id = Column(String(5), ForeignKey("employees.id"))
+
+    lead = relationship("Employee", back_populates="team_ed")
