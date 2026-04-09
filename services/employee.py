0b4f840feee4e09c7d6a68fe1b1fa689ade7d797
TimHerrmannTU <platinum2dollar@gmail.com>
2026-04-09 12:22:00 +0200

    refactor: making big bob proud

--- services/employee.py
+++ services/employee.py
@@ -1,13 +1,15 @@
 from datetime import date
 from collections import defaultdict
-from typing import Optional, Tuple, Dict
+from typing import Optional
 
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session, joinedload
 
+from schemas.calendar import CalendarDayRead
 from schemas.employee import EmployeeDetailedView
 from schemas.employeeHourTarget import EmployeeHourTargetRead
 from schemas.employeeVacationClaim import EmployeeVacationClaimRead
+from schemas.log import DailyLogRead
 
 from crud.log import get_employee_logs, get_employee_logs_within_range
 from crud.calendar import get_calendar_days, get_calendar_days_within_range
@@ -30,7 +32,7 @@ def get_employee_detailed(db: Session, emp_id: str) -> Optional[EmployeeDetailed
     
     return view
     
-def _build_vacation_claim_history(db, emp) -> list[EmployeeVacationClaimRead]:
+def _build_vacation_claim_history(db: Session, emp: EmployeeDetailedView) -> list[EmployeeVacationClaimRead]:
         
     vacation_rules = vacation_contract_crud.get_all(db)
     existing_claims = {claim.year: claim for claim in emp.vacation_claims}
@@ -45,7 +47,7 @@ def _build_vacation_claim_history(db, emp) -> list[EmployeeVacationClaimRead]:
 
     return claim_history
 
-def _generate_virtual_claim(vacation_rules, emp_id, first_year: int, year: int) -> EmployeeVacationClaimRead:
+def _generate_virtual_claim(vacation_rules: list[EmployeeVacationClaimRead], emp_id: str, first_year: int, year: int) -> EmployeeVacationClaimRead:
     seniority = year - first_year
     claim_in_days = _get_claim_by_seniority(vacation_rules, seniority)
     return EmployeeVacationClaimRead(
@@ -65,12 +67,12 @@ def _get_claim_by_seniority(rules: list[EmployeeVacationClaimRead], seniority: i
 # ENDPOINT /logs/{emp_id}/{year}/{month} #
 ##########################################
 
-def get_employee_month_view(db: Session, emp_id: str, year: int, month: int) -> Dict:
+def get_employee_month_view(db: Session, emp_id: str, year: int, month: int) -> dict:
     days = get_calendar_days(db, year, month)
     timesheet = _build_timesheet(db, emp_id, year, month)
     
     first_day = date(year, month, 1)
-    lt_target, lt_actual = _get_employee_lifetime_stats(db, emp_id, first_day)
+    lt_target, lt_actual = _get_employee_hour_stats(db, emp_id, first_day)
 
     return {
         "meta": {
@@ -86,63 +88,15 @@ def get_employee_month_view(db: Session, emp_id: str, year: int, month: int) -> 
         }
     }
     
-def _get_employee_lifetime_stats(db: Session, emp_id: str, calc_end: Optional[date] = None) -> Tuple[float, float]:
-    
-    emp = employee_crud.get_with_details(db, emp_id)
-    if not emp: return 0.0, 0.0
-    
-    contracts = emp.hour_targets
-    
-    calc_start = emp.start_tracking_date
-    if calc_end is None: calc_end = date.today()
-
-    days = get_calendar_days_within_range(db, calc_start, calc_end)
-    logs = get_employee_logs_within_range(db, emp_id, calc_start, calc_end)
-    
-    log_map = {l.date: l for l in logs}
-    
-    actual_sum = sum(sum(p.time for p in log.project_hours) for log in logs)
-    target_sum = sum(c.starting_balance for c in contracts if c.starting_balance)
-
-    for day in days:
-        log = log_map.get(day.date)
-        if log:
-            target_sum += log.target_hours
-            continue
-        
-        if day.is_weekend: continue
-
-        base_target_factor = 1.0
-        if day.holiday: 
-            base_target_factor *= day.holiday.target_factor
-        if base_target_factor == 0.0: continue
-        
-        # Determine relevant contract for the specific date
-        relevant_contract = None
-        if len(contracts) == 1: 
-            relevant_contract = contracts[0]
-        else:
-            for contract in contracts:
-                start_ok = (contract.valid_start is None) or (contract.valid_start <= day.date)
-                stop_ok = (contract.valid_stop is None) or (day.date <= contract.valid_stop)
-                if start_ok and stop_ok:
-                    relevant_contract = contract
-                    break
-        
-        if relevant_contract:
-            spread = relevant_contract.target_spread
-            day_target = relevant_contract.weekly_target * (spread[day.weekday] / sum(spread))
-            target_sum += day_target * base_target_factor
-    
-    return target_sum, actual_sum
-
 def _build_timesheet(db: Session, emp_id: str, year: int, month: int) -> list:
     
-    days = get_calendar_days(db, year, month)
     contract = hour_contract_crud.get_for_month(db, emp_id, year, month)
+    
+    days = get_calendar_days(db, year, month)
     logs = get_employee_logs(db, emp_id, year, month)
     
     log_map = {l.date: l for l in logs}
+    
     timesheet = {
         str(day.date): log_map.get(day.date) or _create_empty_log(day, contract)
         for day in days
@@ -150,7 +104,7 @@ def _build_timesheet(db: Session, emp_id: str, year: int, month: int) -> list:
     
     return timesheet 
 
-def _create_empty_log(day, contract):
+def _create_empty_log(day: CalendarDayRead, contract: EmployeeHourTargetRead):
     tgt = 0.0 # weekend or free
     if day.holiday and contract:
         tgt = contract.weekly_target * day.holiday.target_factor
@@ -169,11 +123,73 @@ def _create_empty_log(day, contract):
         "timeframes": [],
     }
 
+def _get_employee_hour_stats(db: Session, emp_id: str, calc_end: Optional[date] = None) -> tuple[float, float]:
+    
+    emp = employee_crud.get_with_details(db, emp_id)
+    if not emp: return 0.0, 0.0
+    
+    contracts = emp.hour_targets
+    
+    calc_start = emp.start_tracking_date
+    if calc_end is None: calc_end = date.today()
+
+    days = get_calendar_days_within_range(db, calc_start, calc_end)
+    logs = get_employee_logs_within_range(db, emp_id, calc_start, calc_end)
+    
+    log_map = {l.date: l for l in logs}
+    
+    actual_sum = sum(sum(p.time for p in log.project_hours) for log in logs)
+    target_sum = _get_target_sum(days, log_map, contracts)
+    
+    return target_sum, actual_sum
+
+def _get_target_sum(days: list[CalendarDayRead], log_map: dict[date, DailyLogRead], contracts: list[EmployeeHourTargetRead]) -> float:
+    target_sum = sum(c.starting_balance for c in contracts if c.starting_balance)
+    for day in days:
+        log = log_map.get(day.date)
+        if log: 
+            target_sum += log.target_hours
+        else:
+            base_target_factor = _get_target_factor_of_holiday(day)
+            if day.is_weekend or base_target_factor == 0.0: continue # GATE
+            target_sum += _get_target_from_contract(base_target_factor, day, contracts)
+    return target_sum
+
+def _get_target_factor_of_holiday(day: CalendarDayRead) -> float:
+    base_target_factor = 1.0
+    if day.holiday: 
+        base_target_factor *= day.holiday.target_factor
+    return base_target_factor
+
+def _get_target_from_contract(base_target_factor: float, day: CalendarDayRead, contracts: list[EmployeeHourTargetRead]) -> float:
+    hour_target = 0
+    
+    relevant_contract = _find_contract_for_date(day.date, contracts)
+    if relevant_contract:
+        hour_target = _get_hours_from_spread(relevant_contract, day.weekday) * base_target_factor
+
+    return hour_target
+
+def _find_contract_for_date(target_date: date, contracts: list[EmployeeHourTargetRead]) -> EmployeeHourTargetRead | None:
+    for contract in contracts:
+        is_after_start = (contract.valid_start is None) or (contract.valid_start <= target_date)
+        is_before_stop = (contract.valid_stop is None ) or (target_date <= contract.valid_stop)
+        if is_after_start and is_before_stop:
+            return contract
+    return None
+    
+def _get_hours_from_spread(contract: EmployeeHourTargetRead, weekday: int) -> float:
+    spread = contract.target_spread
+    spread_sum = sum(spread)
+    
+    return 0.0 if spread_sum == 0 else contract.weekly_target * (spread[weekday] / spread_sum)
+    
+
 ##################################
 # ENDPOINT /logs/{emp_id}/{year} #
 ##################################
 
-def get_employee_year_view(db: Session, emp_id: str, year: int) -> Dict:
+def get_employee_year_view(db: Session, emp_id: str, year: int) -> dict:
     days = get_calendar_days(db, year)
     logs = get_employee_logs(db, emp_id, year)
     
@@ -198,7 +214,7 @@ def get_employee_year_view(db: Session, emp_id: str, year: int) -> Dict:
 # ENDPOINT /dashboard/employee/{emp_id} #
 #########################################
 
-def get_dashboard(db: Session, emp_id: str, calc_end: Optional[date] = None) -> Dict:
+def get_dashboard(db: Session, emp_id: str, calc_end: Optional[date] = None) -> dict:
     emp = employee_crud.get(db, emp_id)
     if not emp: return {"meta": {"total": 0}, "pros": {}}
 
