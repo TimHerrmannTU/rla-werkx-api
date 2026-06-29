32bf5dc2f205c1bfb0c69b47cd7ebcbacc942726
TimHerrmannTU <platinum2dollar@gmail.com>
2026-06-29 11:03:20 +0200

    chore: updates alembic model imports

--- alembic/env.py
+++ alembic/env.py
@@ -13,12 +13,13 @@ from src.core.config import get_settings
 # Add project root to python path to resolve 'database' and 'models'
 sys.path.append(os.getcwd())
 
-from src.modules.employee.model import Employee
-from src.modules.project.model import Project, ProjectPhase, ProjectFlag
-from src.modules.log.model import LogDailySummary, LogProjectHour
-from src.modules.team.model import Team
 from src.modules.location.model import Location
+from src.modules.employee.model import Employee, EmployeeHourTarget, EmployeeVacationClaim
+from src.modules.employee.config import VacationRule
+from src.modules.project.model import Project, ProjectPhase, ProjectPartial, ProjectService, ProjectFlag
+from src.modules.log.model import LogDailySummary, LogTimeframe, LogProjectHour
 from src.modules.calender.model import CalendarDay, Holiday
+from src.modules.team.model import Team
 
 # Alembic Config object
 config = context.config
